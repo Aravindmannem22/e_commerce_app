22060c4bebd203ee6be1d78989bd992b2395ef88
Aravindmannem22 <aravindmannem22@gmail.com>
2024-03-04 13:54:50 +0530

    app added

---
+++ e-commerceapp/src/main/java/org/jsp/ecommerceapp/ECommerceappApplication.java
@@ -0,0 +1,13 @@
+package org.jsp.ecommerceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceappApplication.class, args);
+	}
+
+}
---
+++ e-commerceapp/src/main/java/org/jsp/ecommerceapp/repository/Merchantrepository.java
@@ -0,0 +1,8 @@
+package org.jsp.ecommerceapp.repository;
+
+import org.jsp.ecommerceapp.model.Merchant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Merchantrepository extends JpaRepository<Merchant, Integer>{
+
+}
